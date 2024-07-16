073c806e371e71b6197d337e2efdb58d4cab5293
ryanphobia <31889853+ryanphobia@users.noreply.github.com>
2024-07-16 23:01:02 +0700

    Update wp-config.php

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -43,7 +43,7 @@ if (isset($_ENV['HOST'])) {
 define( 'DB_CHARSET', 'utf8' );
 
 /** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+define( 'DB_COLLATE', 'utf8mb4_general_ci' );
 
 /**#@+
  * Authentication unique keys and salts.
